e281084162adde270dc4aee62c6baebf9fa9d70f
sid <sidengineer.014@gmail.com>
2024-04-27 02:10:36 +0530

    socket.io server updated

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 
+
 const express = require('express');
 const {Server} = require("socket.io")
 const {createServer} = require("http") 
@@ -13,7 +14,23 @@ const io = new Server(httpServer , {cors : "http://localhost:3000"});
 
 io.on("connection" , (socket) => {
     console.log('socket connected');
+    socket.on('beginPath' , (arg) => {
+       socket.broadcast.emit('beginPath', arg)
+    })
+    socket.on('drawLine', (arg) =>{
+        socket.broadcast.emit('drawLine' , arg)
+    })
+
+    socket.on('config' , (arg) => {
+        console.log(arg);
+        
+        socket.broadcast.emit('config' , arg)
+    })
 })
+
+
+
+
 httpServer.listen(5000 , () =>{
     console.log('server is running on port 5000');
 })
