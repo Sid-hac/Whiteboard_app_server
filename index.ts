2943961c31dc9dbf44523d40c0f48742761d5ddf
sid <sidengineer.014@gmail.com>
2024-06-23 14:15:21 +0530

    frontend whitelisted updated2

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const cors = require("cors")
 
 const app = express();
 
-const isDev = app.settings.env === "development"
+const isDev = app.settings.env === "production"
 const URLL = isDev ? "http://localhost:3000" : "https://doodleboard-new.netlify.app"
 
 app.use(cors({origin : URLL}))
